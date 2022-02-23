670ad6e1b4ff6fb717a479f5f0122f9194290a3b
Xinyu Chen <xinyuchen98@gmail.com>
2022-02-22 22:08:59 -0700

    Load tweets with ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,32 +6,6 @@
  */
 // Test / driver code (temporary). Eventually will get this from the server.
 $(document).ready(() => {
-  // Fake data taken from initial-tweets.json
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
-
   const $tweetsContainer = $('#all-tweets');
 
   const renderTweets = function(tweets) {
@@ -73,6 +47,17 @@ $(document).ready(() => {
     return $tweetElement;
   };
 
+  const loadTweets = () => {
+    $.ajax({
+      url: '/tweets',
+      method: 'GET'
+    }).then((tweets) => {
+      $tweetsContainer.empty();
+      tweets.sort((a,b) => (a.created_at < b.created_at) ? 1 : ((b.created_at < a.created_at) ? -1 : 0));
+      renderTweets(tweets);
+    });
+  };
+
   $("#post-tweet").submit(function(event) {
     event.preventDefault();
     const formData = $(this).serialize();
@@ -84,6 +69,6 @@ $(document).ready(() => {
     });
   });
 
-  renderTweets(data);
+  loadTweets();
 
 });
